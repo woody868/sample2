48441c6cd88d6d99fc82daf82f83c50147b6a5e9
woody868 <lhj303606@gmail.com>
2025-08-07 10:25:22 +0900

    js

--- event.js
+++ event.js
@@ -4,3 +4,10 @@ let btn1 = document.querySelector("#btn1");
 btn1.addEventListener("click", () => {
     console.log("버튼1 클릭");
 });
+
+function greet(name) {
+    return `안녕하세요, ${name}님!`;
+}
+
+console.log(greet("이해주"));  // 콘솔을 통해 호출
+
